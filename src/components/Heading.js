eb5325e90d7ae4e3c111133d8abd3febda8f62df
Zach LaFleur <zach.lafleur95@gmail.com>
2020-12-08 09:46:11 -0700

    initial commit (also created components file within src. as well as header component)

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Heading.css";
+
+function Heading({ title }) {
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+export default Heading
